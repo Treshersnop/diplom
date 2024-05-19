f80deef9a7d9cb897110599db1167f80ef9b8b7a
regina <karimowakarimova@yandex.ru>
2024-05-19 16:04:55 +0500

    добавление отписки и подписки на детальную страницу курса

--- training_course/views/subscription.py
+++ training_course/views/subscription.py
@@ -1,22 +1,21 @@
 from django.contrib.auth.decorators import login_required
+from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponseRedirect
-from django.urls import reverse
-from requests import Request
 
 from training_course import models
 
 
 @login_required
-def create_subscription(request: Request, pk: int):
+def create_subscription(request: WSGIRequest, pk: int) -> HttpResponseRedirect:
     if request.method == 'POST':
         models.Subscription.objects.create(user_id=request.user.id, course_id=pk)
 
-    return HttpResponseRedirect(reverse('training_course:course_list'))
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
 @login_required
-def delete_subscription(request: Request, pk: int):
+def delete_subscription(request: WSGIRequest, pk: int) -> HttpResponseRedirect:
     if request.method == 'POST':
         models.Subscription.objects.filter(user_id=request.user.id, course_id=pk).delete()
 
-    return HttpResponseRedirect(reverse('training_course:course_list'))
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
