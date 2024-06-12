ca5b7137adb76cbabc1d415981102bb60d4b366f
regina <karimowakarimova@yandex.ru>
2024-06-12 22:04:42 +0500

    при ошибке в создании теста с вопросами тест не создастся

--- training_course/tasks.py
+++ training_course/tasks.py
@@ -1,15 +1,14 @@
 from io import BytesIO
 
 from celery import shared_task
+from django.db import transaction
 
-# from project import celery_app
 from training_course import datatools, models
 
 
-# @celery_app.task
 @shared_task
 def create_test(test_file_name: str, test_file: bytes, lesson_id: int) -> None:
-    test = models.Test.objects.create(lesson_id=lesson_id)
+    test = models.Test(lesson_id=lesson_id)
     # try:
     #     questions, answers = parse_docx_test(test_file, test) if test_file.name.endswith('docx') else (
     #         parse_excel_test(test_file, test)
@@ -25,6 +24,7 @@ def create_test(test_file_name: str, test_file: bytes, lesson_id: int) -> None:
         if test_file_name.endswith('docx')
         else (datatools.parse_excel_test(test_file, test))
     )
-
-    models.Question.objects.bulk_create(questions)
-    models.Answer.objects.bulk_create(answers)
+    with transaction.atomic():
+        test.save()
+        models.Question.objects.bulk_create(questions)
+        models.Answer.objects.bulk_create(answers)
